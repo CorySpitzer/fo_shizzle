86ad9abc73f0821b16ac37cede61c8f32bd39726
Guest User <student@epicodus.com>
2015-04-20 15:24:58 -0700

    Refactor fo shizzle logic for simplicity; pass all tests. Start tracking lib/fo_shizzle.rb.

---
+++ lib/fo_shizzle.rb
@@ -0,0 +1,24 @@
+
+
+class String
+  define_method(:fo_shizzle) do
+    words = self.split(" ")
+    shizzle_out = []
+    words.each() do |word|
+      letters = word.split("")
+      shizzle_word = ""
+
+      first_letter = true
+      letters.each do |letter|
+        if first_letter == true or letter != "s"
+          shizzle_word.concat(letter)
+          first_letter = false
+        else
+          shizzle_word.concat("z")
+        end
+      end
+      shizzle_out.push(shizzle_word)
+    end
+    shizzle_out.join(" ")
+  end
+end
